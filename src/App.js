4bd7cecaaf05f7b8e03e379e611704327b1acd32
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2024-05-26 19:19:13 -0300

    Ajustes na responsividade da seção principal + elaboração do menu mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/header'
+import MobileHeader from './components/mobile_header'
 import Body from './components/body'
 import Footer from './components/footer';
 import "../src/index.css"
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="project-content">
       <Header/>
+      <MobileHeader/>
       <Body/>
       <Footer/>
     </div>
---
+++ src/components/mobile_header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+const header_mobile = ()=>{
+    return(    
+    <header class="portifolio-header-mobile">
+    <ul className='flex'>
+        <li>
+            <a>PROJETOS PESSOAIS</a>
+        </li>
+        <li>
+            <a>PORTIFÓLIO</a>
+        </li>
+        <li>
+            <a>SOBRE</a>
+        </li>
+        <li>
+            <a>CONTATO</a>
+        </li>
+    </ul>
+</header>)
+
+}
+
+export default header_mobile
+
