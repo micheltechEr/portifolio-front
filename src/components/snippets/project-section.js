f273d383f9b921562d1b2811012001e10ea1f3bb
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2024-06-20 00:16:54 -0300

    Construção parte 1 da seção de projetos pessoais

--- src/components/snippets/project-section.js
+++ src/components/snippets/project-section.js
@@ -1,19 +1,41 @@
 import React from "react";
-
+import APODImage from '../../img/apod.png'
+import NetflixClone from '../../img/netflix-code.png'
+import ForBarber from '../../img/forbarber.png'
 const ProjectSection = ()=>{
     return(
         <div className="personal-projects" id="personal-projects">
-            <h2>Projetos Pessoais</h2>
+            <h2>
+                Projetos Pessoais
+                <span className="arrows">&larr;	&rarr;</span>
+            </h2>
+
             <div className="box-section">
                 <div className="group-projects">
                     <div className="item">
-                        <p>&#123;APOD&#125;</p>
+                        <span>&#123;APOD&#125;</span>
+                        <div className="project-image">
+                            <a href="https://daily-apod-nasa.vercel.app/" target="_blank">
+                            <img src={APODImage}/>   
+                            </a>
+                                      
+                        </div>
                     </div>
                     <div className="item">
-                        <p>&#123;Clone Home Netflix&#125;</p>
+                        <span>&#123;Clone Home Netflix&#125;</span>
+                        <div className="project-image">
+                            <a href="https://netflix-clone-dio-sigma.vercel.app/">
+                            <img src={NetflixClone}/> 
+                            </a>             
+                        </div>
                     </div>
                     <div className="item">
-                        <p>&#123;ForBarber&#125;</p>
+                        <span>&#123;ForBarber&#125;</span>
+                        <div className="project-image">
+                            <a href="https://github.com/micheltechEr/forbarber">
+                            <img src={ForBarber}/>             
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
