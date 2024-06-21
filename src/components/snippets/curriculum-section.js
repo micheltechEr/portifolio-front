462c0424be88c688cc175401d1f0a50fce22e13d
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2024-06-21 00:17:53 -0300

    Construção parte 2 para mobie de projetos pessoais + adição de efeitos para desktop + andamento da parte de curriculum

--- src/components/snippets/curriculum-section.js
+++ src/components/snippets/curriculum-section.js
@@ -3,11 +3,13 @@ import curriculumimage from '../../img/curriculum.png'
 const CurriculumSection = ()=>{
     return(
         <div className="curriculum-section" id="curriculum-section">
-            <h2>Portifólio e Curriculo</h2>
+            <div className="box-description">
+            <h2>Portifólio & Curriculo</h2>
             <h3>Aqui listei minha formação acadêmica e minhas habilidades técnicas.
                 A medida que aprimoro, adiciono novos “objetos”
                 de habilidades.
             </h3>
+            </div>
             <div className="box-image">
                 <img src={curriculumimage} alt="curriculum-image" />
             </div>
