4689f2d710cb095007bae02feabe90cd7e6469b9
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2024-07-07 00:58:47 -0300

    Foi colocado o menu superior como fixado,ajuste das demais seções tanto mobile quanto desktop,ajuste no menu mobile com a animação deixando mais fluída,inicio do ajuste do rodapé,separação das cores através de código root para eventuais mudanças via javascript,inicio dos ajustes para o modo dark.Posteriormente serão adicionadas mais funções e inicio da página no modo dark + outras funcionalidades

--- src/components/snippets/about-me.js
+++ src/components/snippets/about-me.js
@@ -6,10 +6,10 @@ const AboutMe = ()=>{
         <div className="about-me">
             <div className="about-me-text" id="about-me-text">
                 <h2>Sobre mim</h2>
-                <h4>
+                <h3>
                 E aí, galera!
-                Sou Ângelo Miguel, de Amélia Rodrigues, Bahia, com 24 anos de pura paixão pela tecnologia. Formado em Sistemas de Informação e trabalhando como desenvolvedor front-end, estou sempre pronto para mergulhar de cabeça em novos desafios?
-                </h4>
+                Sou Ângelo Miguel, de Amélia Rodrigues, Bahia, com 24 anos de pura paixão pela tecnologia. Formado em Sistemas de Informação e trabalhando como desenvolvedor front-end, estou sempre pronto para mergulhar de cabeça em novos desafios.
+                </h3>
             </div>
             <div className="about-me-image">
                 <img src={profilecurriculum} alt="profile-picture-curriculum"/>
--- src/functions/main.js
+++ src/functions/main.js
@@ -7,17 +7,14 @@ const showMobileMenu = () => {
             document.querySelector('.toggle-burger').style.display = 'none';
         }
     
-        // Function to hide the mobile menu and shadow effect
         function hideMenu() {
-            document.querySelector('.portifolio-header-mobile ul').classList.remove('active');
-            document.querySelector('.shadowEffect').classList.remove('shadowOn');
-            document.querySelector('.toggle-burger').style.display = 'block';
+            let menu = document.querySelector('.portifolio-header-mobile ul');
+            menu.classList.remove('active');
+                document.querySelector('.shadowEffect').classList.remove('shadowOn');
+                document.querySelector('.toggle-burger').style.display = 'block';
         }
     
-        // Add click event to the burger icon to show the menu
         document.querySelector('.toggle-burger svg').addEventListener('click', showMenu);
-    
-        // Add click event to the shadow effect to hide the menu
         document.querySelector('.shadowEffect').addEventListener('click', hideMenu);
     });
 
